3929d05deb26333b48bbddc981fa1055da7d9095
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-08 23:15:46 +0200

    Add server.properties to gitignore

--- start-server.bash
+++ start-server.bash
@@ -6,4 +6,4 @@ PROMPT_COMMAND='echo -en "\033]0; steve-soundless-tech-modpack-minecraft - 1.18.
 ./start-packwiz-server.bash &
 
 java -jar packwiz-installer-bootstrap.jar -g -s server http://lineofflat.duckdns.org:24465/pack.toml
-java -jar -Xms8196m -Xmx8196m quilt-server-launch.jar nogui
+java -jar -Xms6144m -Xmx6144m quilt-server-launch.jar nogui
--- start-server.bat
+++ start-server.bat
@@ -4,6 +4,6 @@ title "steve-soundless-tech-modpack-minecraft - 1.18.2 Quilt Minecraft server"
 start start-packwiz-server.bat
 
 "C:\Program Files\Eclipse Adoptium\jdk-17.0.2.8-hotspot\bin\java.exe" -jar packwiz-installer-bootstrap.jar -g -s server http://lineofflat.duckdns.org:24465/pack.toml
-"C:\Program Files\Eclipse Adoptium\jdk-17.0.2.8-hotspot\bin\java.exe" -jar -Xms8196m -Xmx8196m quilt-server-launch.jar nogui
+"C:\Program Files\Eclipse Adoptium\jdk-17.0.2.8-hotspot\bin\java.exe" -jar -Xms6144m -Xmx6144m quilt-server-launch.jar nogui
 
 pause
