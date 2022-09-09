55b09553b095f11a98fae740fc75978f260d7ffe
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-10 00:01:57 +0200

    Remove quotes from echo in start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -7,5 +7,5 @@ start start-packwiz-server.bat
 java -jar packwiz-installer-bootstrap.jar -g -s server http://lineofflat.duckdns.org:24465/pack.toml
 java -jar -Xms6144m -Xmx6144m -XX:ParallelGCThreads=4 -XX:+UseCodeCacheFlushing -XX:+UseStringDeduplication ./server/quilt-server-launch.jar nogui
 
-echo "Server stopped."
+echo Server stopped.
 pause
