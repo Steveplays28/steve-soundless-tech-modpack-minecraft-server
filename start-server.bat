4ec0f3e12be212edc3690f72ed0642185266381c
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-10 00:09:20 +0200

    Fix launch error

--- start-server.bat
+++ start-server.bat
@@ -3,9 +3,10 @@
 title steve-soundless-tech-modpack-minecraft - 1.18.2 Quilt Minecraft server
 
 start start-packwiz-server.bat
-
 java -jar packwiz-installer-bootstrap.jar -g -s server http://lineofflat.duckdns.org:24465/pack.toml
-java -jar -Xms6144m -Xmx6144m -XX:ParallelGCThreads=4 -XX:+UseCodeCacheFlushing -XX:+UseStringDeduplication ./server/quilt-server-launch.jar nogui
+
+cd server/
+java -jar -Xms6144m -Xmx6144m -XX:ParallelGCThreads=4 -XX:+UseCodeCacheFlushing -XX:+UseStringDeduplication quilt-server-launch.jar nogui
 
 echo Server stopped.
 pause
--- start-server.sh
+++ start-server.sh
@@ -4,11 +4,14 @@
 title="steve-soundless-tech-modpack-minecraft - 1.18.2 Quilt Minecraft server"
 echo -e '\033]2;'$title'\007'
 
-# Start Packwiz in new window
+# Start Packwiz server in new window
 nohup ./start-packwiz-server.bash &
 
-# Start Minecraft server
+# Start Packwiz client
 java -jar packwiz-installer-bootstrap.jar -g -s server http://lineofflat.duckdns.org:24465/pack.toml
-java -jar -Xms6144m -Xmx6144m -XX:ParallelGCThreads=4 -XX:+UseCodeCacheFlushing -XX:+UseStringDeduplication ./server/quilt-server-launch.jar nogui
+
+# Start Minecraft server
+cd server/
+java -jar -Xms6144m -Xmx6144m -XX:ParallelGCThreads=4 -XX:+UseCodeCacheFlushing -XX:+UseStringDeduplication quilt-server-launch.jar nogui
 
 read -p "Server stopped."
