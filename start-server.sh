9b910b2b6b59a645fe5f3d8e7c6daf3eb604b1e3
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-18 11:51:08 +0200

    Separate Packwiz from server start script

--- start-server.sh
+++ start-server.sh
@@ -4,9 +4,6 @@
 title="steve-soundless-tech-modpack-minecraft - 1.18.2 Quilt Minecraft server"
 echo -e '\033]2;'$title'\007'
 
-# Start Packwiz server in new window
-nohup ./start-packwiz-server.bash &
-
 # Start Packwiz client and Minecraft server
 cd server/
 java -jar packwiz-installer-bootstrap.jar -g -s server http://lineofflat.duckdns.org:24465/pack.toml
